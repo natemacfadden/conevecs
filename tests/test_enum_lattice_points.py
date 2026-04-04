c116838b92eb38aab5008e22f5ba4fe274b4df94
Nate MacFadden <natemacfadden@gmail.com>
2026-04-04 12:33:22 -0400

    update authorship

--- tests/test_enum_lattice_points.py
+++ tests/test_enum_lattice_points.py
@@ -1,5 +1,5 @@
 # =============================================================================
-#    Copyright (C) 2026  Liam McAllister Group
+#    Copyright (C) 2026  Nate MacFadden for the Liam McAllister Group
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
